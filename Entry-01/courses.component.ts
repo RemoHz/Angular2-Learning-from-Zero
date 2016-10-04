d59bb2fd05471d7c7e972d089d6eda33d585cb8d
Remo <remocode@gmail.com>
2016-10-04 22:46:02 +1100

    Add Directive Part

--- Entry-01/courses.component.ts
+++ Entry-01/courses.component.ts
@@ -1,19 +1,22 @@
 
 import {Component} from 'angular2/core'
 import {CourseService} from './course.service'
+import {AutoGrowDirective} from './auto-grow.directive'
 
 @Component({
     selector: 'courses',
     template: `
         <h2>Courses</h2>
         {{ title }}
+        <input type="text" autoGrow />
         <ul>
             <li *ngFor="#course of courses">
             {{ course }}
             </li>
         </ul>
         `,
-    providers: [CourseService]
+    providers: [CourseService],
+    directives: [AutoGrowDirective]
 })
 
 export class CoursesComponent {
